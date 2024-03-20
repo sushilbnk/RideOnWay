a9c1928d5d2f7359863f9dc528b8b0afa4c48a65
vasavimekala <161515103+vasavimekala@users.noreply.github.com>
2024-03-20 01:10:39 -0500

    Model Creation in models.py

--- SoftwareEngg/RideOnWay/models.py
+++ SoftwareEngg/RideOnWay/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-
-# Create your models here.
-
-
+#Model Creation
 class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=10)
     phoneNumber = models.CharField(max_length=10)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
     userId = models.AutoField(primary_key=True)

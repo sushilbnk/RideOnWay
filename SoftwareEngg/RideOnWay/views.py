dd8329802a614415bf2ecd5fd029fd5686449ca7
kanjarlamanvitharao <manvitharao123abc@gmail.com>
2024-04-01 12:11:31 -0500

    Updated  views.py

--- SoftwareEngg/RideOnWay/views.py
+++ SoftwareEngg/RideOnWay/views.py
@@ -118,3 +118,105 @@ def createRoutes(source, destination, stopList, rideId, isRideAvailable):
                 )
                 rideSourceDestinationDetails.save()
 
+def rideDetails(request, rideId):
+    passengerId = request.COOKIES['id']
+    if request.method == "POST":
+        rideConfirmation = request.POST.get("wantToRide")
+        if rideConfirmation == "yes":
+            rideDictionary = getRideDetailsUsingRideId(rideId, passengerId)
+            rideDetailsByRideId = RideDetails.objects.filter(rideId=rideId).first()
+            rideDetailsByRideId.numberOfPassengersLeft -= 1
+            rideDetailsByRideId.save()
+            if rideDetailsByRideId.numberOfPassengersLeft != 0:
+                newRide = RideDetails.objects.create(
+                    numberOfPassengersLeft=0,
+                    user_id=rideDictionary["userId"],
+                    passenger=passengerId,
+                    isSeatAvailable=False,
+                    time=rideDictionary["time"],
+                    date=rideDictionary["date"],
+                    cost=rideDictionary["cost"]
+                )
+                newRide.save()
+                newRideSourceDestinationDetails = RideSourceDestinationDetails.objects.create(
+                    source=rideDictionary["source"],
+                    destination=rideDictionary["destination"],
+                    ride_id=newRide.rideId,
+                    isRideAvailable=False)
+                newRideSourceDestinationDetails.save()
+            else:
+                rideDetailsByRideId.passenger_id = passengerId
+                rideDetailsByRideId.isSeatAvailable = False
+                rideSourceDestinationDetails = RideSourceDestinationDetails.objects.filter(ride_id=rideId).first()
+                rideSourceDestinationDetails.isRideAvailable = False
+                rideSourceDestinationDetails.save()
+                rideDetailsByRideId.save()
+        return redirect("../../DashBoard/")
+    try:
+        rideDictionary = getRideDetailsUsingRideId(rideId, passengerId)
+        return render(request, 'HTML/RideDetails.html', {"rideDetailsDictionary": rideDictionary})
+    except:
+        return redirect('../Error/')
+
+
+def getRideDetailsUsingRideId(rideId, passengerId):
+    rideDetailsByRideId = RideDetails.objects.filter(rideId=rideId).first()
+    rideSourceDestinationDetails = RideSourceDestinationDetails.objects.filter(ride_id=rideId).first()
+    user = User.objects.filter(userId=rideDetailsByRideId.user_id).first()
+    presentRide = RideDetails.objects.filter(user_id=rideDetailsByRideId.user_id, passenger=passengerId).first()
+    ownRide = RideDetails.objects.filter(user_id=passengerId, rideId=rideId).first()
+    selected = False
+    if presentRide is not None or ownRide is not None:
+        selected = True
+    rideDictionary = {
+        "rideId": rideId,
+        "source": rideSourceDestinationDetails.source,
+        "destination": rideSourceDestinationDetails.destination,
+        "name": user.name,
+        "userId": user.userId,
+        "numberOfPassengersLeft": rideDetailsByRideId.numberOfPassengersLeft,
+        "date": rideDetailsByRideId.date,
+        "time": rideDetailsByRideId.time,
+        "cost": rideDetailsByRideId.cost,
+        "selected": selected
+    }
+    return rideDictionary
+
+
+def requestRide(request):
+    placeholder = "Enter_the_Destination"
+    isDestinationPresent = False
+    if request.method == "POST":
+        destinationSearch = request.POST.get('searchText')
+        listOfRides = []
+        if destinationSearch != '':
+            placeholder = destinationSearch
+            isDestinationPresent = True
+            rideSourceDestinationDetails = RideSourceDestinationDetails.objects.filter(destination=destinationSearch)
+            for i in rideSourceDestinationDetails:
+                rideId = i.ride_id
+                rideDetailsByRideId = RideDetails.objects.filter(rideId=rideId).first()
+                if rideDetailsByRideId.isSeatAvailable:
+                    numberOfPassengersLeft = rideDetailsByRideId.numberOfPassengersLeft
+                    user = User.objects.filter(userId=rideDetailsByRideId.user_id).first()
+                    listOfRides.append(
+                        {"source": i.source,
+                         "destination": destinationSearch,
+                         "name": user.name,
+                         "numberOfPassengersLeft": numberOfPassengersLeft,
+                         "time": rideDetailsByRideId.time,
+                         "date": rideDetailsByRideId.date,
+                         "rideId": rideId
+                         }
+                    )
+        return render(request, 'HTML/RequestRide.html',
+                      {"isDestinationPresent": isDestinationPresent, "listOfRides": listOfRides,
+                       "placeHolderValue": placeholder})
+    return render(request, 'HTML/RequestRide.html',
+                  {"placeHolderValue": placeholder, "isDestinationPresent": isDestinationPresent})
+
+
+def driverDetails(request, userId):
+    user = User.objects.filter(userId=userId).first()
+    userDetails = {"name": user.name,"phoneNumber": user.phoneNumber}
+    return render(request, "HTML/DriverDetails.html",{"userDetails":userDetails})

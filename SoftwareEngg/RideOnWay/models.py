31e3bf7446b03e9b0bf72f1a8eb6b6695bc560e8
sushilbnk <sushilbanka84@gmail.com>
2024-04-01 10:35:55 -0500

    Modified models.py

--- SoftwareEngg/RideOnWay/models.py
+++ SoftwareEngg/RideOnWay/models.py
@@ -7,3 +7,21 @@ class User(models.Model):
     phoneNumber = models.CharField(max_length=10)
     name = models.CharField(max_length=150)
     userId = models.AutoField(primary_key=True)
+    
+class RideDetails(models.Model):
+    rideId = models.AutoField(primary_key=True)
+    numberOfPassengersLeft = models.IntegerField()
+    user = models.ForeignKey(User, to_field='userId', on_delete=models.CASCADE)
+    passenger = models.IntegerField(null=True)
+    isSeatAvailable = models.BooleanField()
+    time = models.CharField(max_length=20, null=True)
+    date = models.CharField(max_length=100, null=True)
+    cost = models.IntegerField(default=0)
+
+
+class RideSourceDestinationDetails(models.Model):
+    source = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+    sourceDestinationId = models.AutoField(primary_key=True)
+    ride = models.ForeignKey(RideDetails, to_field='rideId', on_delete=models.CASCADE)
+    isRideAvailable = models.BooleanField()

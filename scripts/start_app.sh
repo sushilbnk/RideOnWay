f06e24298e54a4adc64ce52ba1e6bdb7fb14715c
sushilbnk <154746583+sushilbnk@users.noreply.github.com>
2024-04-29 18:07:39 -0500

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/RIDEONWAY-RIDE/SoftwareEngg/settings.py
+sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/RideOnWay/SoftwareEngg/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     

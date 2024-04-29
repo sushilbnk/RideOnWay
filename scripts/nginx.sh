2faa9797844837d1099a101bc3db2aa9803694d8
sushilbnk <154746583+sushilbnk@users.noreply.github.com>
2024-04-29 18:31:46 -0500

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -5,8 +5,8 @@ sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/RideOnWay/nginx/nginx.conf /etc/nginx/sites-available/SoftwareEngg
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/SoftwareEngg /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/SoftwareEngg /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
